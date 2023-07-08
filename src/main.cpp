9ef2f6923a8cccb3b3482943fe5136afa4090470
yus <julznc@yahoo.com>
2023-07-08 21:52:07 +0800

    import Adafruit libraries (https://github.com/adafruit)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_SH110X.h>
+#include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+#define SCREEN_WIDTH            128   // OLED display width, in pixels
+#define SCREEN_HEIGHT           64    // OLED display height, in pixels
+#define SCREEN_SDA_PIN          (9)
+#define SCREEN_SCL_PIN          (10)
+
+#define LED_STRIP_PIN           (21)
+#define LED_STRIP_NUMPIXELS     (8 * 8 * 2)
+
+
+Adafruit_SH1106G oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+Adafruit_NeoPixel pixels(LED_STRIP_NUMPIXELS, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Wire.setPins(SCREEN_SDA_PIN, SCREEN_SCL_PIN);
+    oled.begin();
+
+    oled.display();
+    pixels.begin();
+    pixels.fill(pixels.Color(0, 0, 0), 0, LED_STRIP_NUMPIXELS);
+    pixels.show();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
 }
