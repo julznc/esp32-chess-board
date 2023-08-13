d99087bc8c56d2e8a50b755d38bd5c5064016ecb
yus <julznc@yahoo.com>
2023-08-13 11:55:17 +0800

    basic validation of starting position

--- src/chess/chess.cpp
+++ src/chess/chess.cpp
@@ -15,9 +15,10 @@ static move_st          pending_move;
 
 static const uint8_t   *pu8_pieces = NULL;
 static uint8_t          au8_prev_pieces[64];
-static char             fen_buf[80];
-static bool             pending_led = false;
-static bool             skip_start_fen = false;
+static char             ac_fen_buf[80];
+static bool             b_pending_led = false;
+static bool             b_skip_start_fen = false;
+static bool             b_valid_posision = false;
 
 static struct {
     char san_black[16];
@@ -48,9 +49,105 @@ static inline void unlock(void)
     (void)xSemaphoreGive(mtx);
 }
 
-static inline void load_position(const uint8_t *raw, bool initial)
+static inline uint8_t count_pieces(const uint8_t u8_piece, uint8_t *pau8_sqs=NULL, uint8_t u8_max=0)
 {
+    uint8_t u8_count = 0;
 
+    for (uint8_t rank = 0; rank < 8; rank++)
+    {
+        for (uint8_t file = 0; file < 8; file++)
+        {
+            uint8_t sq = ((7-rank) << 4) + file;
+            if (u8_piece == st_game.board[sq])
+            {
+                if ((u8_count < u8_max) && (NULL != pau8_sqs)) {
+                    pau8_sqs[u8_count] = sq;
+                }
+                u8_count++;
+            }
+        }
+    }
+
+    return u8_count;
+}
+
+static inline bool validate_king(void)
+{
+    if (1 != count_pieces(MAKE_PIECE(WHITE, KING)))
+    {
+        LOGW("invalid white king");
+    }
+    else if (1 != count_pieces(MAKE_PIECE(BLACK, KING)))
+    {
+        LOGW("invalid black king");
+    }
+    else
+    {
+        uint8_t sqw = st_game.stats.kings[WHITE];
+        uint8_t sqb = st_game.stats.kings[BLACK];
+        uint8_t rw = RANK(sqw), fw = FILE(sqw);
+        uint8_t rb = RANK(sqb), fb = FILE(sqb);
+        uint8_t rdiff = rw > rb ? (rw - rb) : (rb - rw);
+        uint8_t fdiff = fw > fb ? (fw - fb) : (fb - fw);
+        if ((rdiff > 1) || (fdiff > 1)) {
+            // to do: validate king attackers
+            return true;
+        }
+        LOGW("close kayo? white K%c%u black K%c%u", ALGEBRAIC(sqw), ALGEBRAIC(sqb));
+    }
+
+    return false;
+}
+
+static inline bool validate_pawns(uint8_t color)
+{
+    uint8_t au8_sqs[8];
+    uint8_t u8_cnt = 0;
+    bool    b_valid = false;
+
+    if ((u8_cnt = count_pieces(MAKE_PIECE(color, PAWN), au8_sqs, sizeof(au8_sqs))) > 8)
+    {
+        LOGW("too many %s pawns", color_to_string(color));
+    }
+    else
+    {
+        b_valid = true;
+        for (uint8_t i = 0; i < u8_cnt; i++) {
+            uint8_t rank = RANK(au8_sqs[i]);
+            if ((RANK_1 == rank) || ((RANK_8 == rank))) {
+                LOGW("invalid %s pawn at %c%u", color_to_string(color), ALGEBRAIC(au8_sqs[i]));
+                b_valid = false;
+                break;
+            }
+        }
+    }
+
+    return b_valid;
+}
+
+static inline bool validate_position(void)
+{
+    bool    b_valid = false;
+
+    if (!validate_king())
+    {
+        //
+    }
+    else if (!validate_pawns(WHITE) || !validate_pawns(BLACK))
+    {
+        //
+    }
+    else
+    {
+        LOGD("white K%c%u black K%c%u", ALGEBRAIC(st_game.stats.kings[WHITE]), ALGEBRAIC(st_game.stats.kings[BLACK]));
+        b_valid = true;
+    }
+
+    return b_valid;
+}
+
+static inline bool load_position(const uint8_t *raw, bool initial)
+{
     if (initial)
     {
         memset(&st_game, 0, sizeof(st_game));
@@ -61,15 +158,15 @@ static inline void load_position(const uint8_t *raw, bool initial)
         for (uint8_t file = 0; file < 8; file++)
         {
             // note: inverted rank order
-            uint8_t idx = ((7-rank) << 4) + file;
+            uint8_t sq = ((7-rank) << 4) + file;
             uint8_t piece = *raw++;
-            st_game.board[idx] = piece;
+            st_game.board[sq] = piece;
             if (KING == PIECE_TYPE(piece)) {
                 // one king only per color
                 // assert(0 == st_game.stats.kings[PIECE_COLOR(piece)]);
-                st_game.stats.kings[PIECE_COLOR(piece)] = idx;
+                st_game.stats.kings[PIECE_COLOR(piece)] = sq;
             }
-            //printf("%3d=%02x ", idx, st_game.board[idx]);
+            //printf("%3d=%02x ", sq, st_game.board[sq]);
         }
         //printf("\r\n");
     }
@@ -117,6 +214,8 @@ static inline void load_position(const uint8_t *raw, bool initial)
     }
 
     LOGD("fen: %s", generate_fen(&st_game));
+
+    return validate_position();
 }
 
 void init(void)
@@ -178,12 +277,12 @@ static inline void show_checked(void)
 
 static inline void show_pending(void)
 {
-    pending_led = !pending_led; // blink
+    b_pending_led = !b_pending_led; // blink
 
     if (pending_move.piece)
     {
-        ui::leds::setColor(pending_move.from, pending_led ? ui::leds::LED_OFF : ui::leds::LED_GREEN);
-        ui::leds::setColor(pending_move.to, pending_led ? ui::leds::LED_GREEN : ui::leds::LED_OFF);
+        ui::leds::setColor(pending_move.from, b_pending_led ? ui::leds::LED_OFF : ui::leds::LED_GREEN);
+        ui::leds::setColor(pending_move.to, b_pending_led ? ui::leds::LED_GREEN : ui::leds::LED_OFF);
     }
 }
 
@@ -192,29 +291,63 @@ static inline void show_move(void)
     if (last_move.piece)
     {
         ui::leds::setColor(last_move.from, ui::leds::LED_YELLOW);
-        if (pending_move.piece && (pending_move.to == last_move.from) && pending_led) {
+        if (pending_move.piece && (pending_move.to == last_move.from) && b_pending_led) {
             ui::leds::setColor(last_move.from, ui::leds::LED_GREEN);
         }
 
         ui::leds::setColor(last_move.to, ui::leds::LED_YELLOW);
-        if (pending_move.piece && (pending_move.to == last_move.to) && pending_led) {
+        if (pending_move.piece && (pending_move.to == last_move.to) && b_pending_led) {
             ui::leds::setColor(last_move.to, ui::leds::LED_GREEN);
         }
     }
 }
 
-static inline void display_stats(void)
+static inline void display_stats(const char *last_san)
 {
     const stats_st *stats = &st_game.stats;
+    char tmp_fen[24];
+    char *fen = tmp_fen;
 
-    DISPLAY_TEXT1(0, 32, "%c %u %u",
-                WHITE == stats->turn ? 'w' : 'b',
-                stats->half_moves, stats->move_number);
+    *fen++ = stats->turn == WHITE ? 'w' : 'b';
+    *fen++ = ' ';
+    if ((0 == stats->castling[WHITE]) && (0 == stats->castling[BLACK])) {
+        *fen++ = '-';
+    }
+    else {
+        if (stats->castling[WHITE] & BIT_KSIDE_CASTLE)
+            *fen++ = 'K';
+        if (stats->castling[WHITE] & BIT_QSIDE_CASTLE)
+            *fen++ = 'Q';
+        if (stats->castling[BLACK] & BIT_KSIDE_CASTLE)
+            *fen++ = 'k';
+        if (stats->castling[BLACK] & BIT_QSIDE_CASTLE)
+            *fen++ = 'q';
+    }
+
+    *fen++ = ' ';
+    if (!stats->ep_square) {
+        *fen++ = '-';
+    } else {
+        *fen++ = 'a' + FILE(stats->ep_square);
+        *fen++ = '0' + 8 - RANK(stats->ep_square);
+    }
+
+    *fen++ = ' ';
+    fen += snprintf(fen, 4, "%u", stats->half_moves);
+
+    *fen++ = ' ';
+    fen += snprintf(fen, 4, "%u", stats->move_number);
+
+    DISPLAY_CLEAR_ROW(20, 8);
+    DISPLAY_TEXT1(0, 20, "%.*s. %.*s", 14, tmp_fen, 6, last_san);
+
+#if 0 // to do
     DISPLAY_CLEAR_ROW(44, 20);
     //DISPLAY_TEXT2(0, 44, "%s %s", san_white, san_black);
     DISPLAY_TEXT2(0, 44, "%.15s %.15s",
                 s_move_stack[(WHITE == stats->turn) && (stats->move_number > 1) ? (stats->move_number - 2) : (stats->move_number - 1)].san_white,
                 s_move_stack[(stats->move_number > 1) ? (stats->move_number - 2) : (stats->move_number - 1)].san_black);
+#endif
 }
 
 static inline void do_move(move_st *list, move_st *move)
@@ -254,7 +387,7 @@ static inline void do_move(move_st *list, move_st *move)
         strcat(san_buf, "sm"); // stalemate
     }
 
-    LOGD("%-5s : \"%s\"", san_buf, generate_fen(&st_game));
+    LOGD("%-5s : %s", san_buf, generate_fen(&st_game));
     if (BLACK == st_game.stats.turn) {
         strncpy(s_move_stack[st_game.stats.move_number - 1].san_white, san_buf, 15);
     } else {
@@ -262,26 +395,19 @@ static inline void do_move(move_st *list, move_st *move)
     }
     //DISPLAY_CLEAR();
     //DISPLAY_TEXT(4, 48, 1, "%s", san_buf);
-    display_stats();
+    display_stats(san_buf);
 }
 
 static inline bool check_start_fen(void)
 {
     uint8_t u8_diff = 0;
 
-    if (ui::btn::pb1.getCount())
+    for (uint8_t u8_idx = 0; u8_idx < sizeof(AU8_START_PIECES); u8_idx++)
     {
-        skip_start_fen = true;
-    }
-    else
-    {
-        for (uint8_t u8_idx = 0; u8_idx < sizeof(AU8_START_PIECES); u8_idx++)
+        if (pu8_pieces[u8_idx] != AU8_START_PIECES[u8_idx])
         {
-            if (pu8_pieces[u8_idx] != AU8_START_PIECES[u8_idx])
-            {
-                ui::leds::setColor(u8_idx>>3, u8_idx&7, ui::leds::LED_RED);
-                u8_diff++;
-            }
+            ui::leds::setColor(u8_idx>>3, u8_idx&7, ui::leds::LED_RED);
+            u8_diff++;
         }
     }
 
@@ -292,20 +418,33 @@ static inline bool check_start_fen(void)
 void loop(void)
 {
     static uint32_t u32_last_checked = 0;
-    static bool     b_start_fen = false;
 
     uint8_t au8_allowed_squares[28];
     uint8_t u8_squares_count;
 
     ui::leds::clear();
 
-    if (false == b_start_fen)
+    if (!st_game.history) // if no moves yet
     {
-        b_start_fen = skip_start_fen || check_start_fen();
-        if (b_start_fen)
+        // if upper-left button was pressed ...
+        if ((millis() - u32_last_checked > 1000UL) && (1 == ui::btn::pb1.getCount()))
+        {
+            b_skip_start_fen = true; // allow custom position
+            if (b_valid_posision) {
+                st_game.stats.turn = SWAP_COLOR(st_game.stats.turn); // toggle turn
+                LOGD("new fen: %s", generate_fen(&st_game));
+            }
+            u32_last_checked = millis();
+        }
+    }
+
+    if (false == b_valid_posision)
+    {
+        if (b_skip_start_fen || check_start_fen())
         {
             memcpy(au8_prev_pieces, pu8_pieces, sizeof(au8_prev_pieces));
-            load_position(pu8_pieces, true);
+            b_valid_posision = load_position(pu8_pieces, true);
+            b_skip_start_fen = b_valid_posision;
         }
     }
     else if (0 == memcmp(au8_prev_pieces, pu8_pieces, sizeof(au8_prev_pieces)))
@@ -313,7 +452,7 @@ void loop(void)
         //LOGD("no change yet");
         if ((0 == last_move.piece) && (0 == pending_move.piece)) {
             show_turn();
-            display_stats();
+            display_stats("...");
         } else if (1) { //!hide_moves) {
             show_pending();
             show_move();
@@ -390,11 +529,11 @@ void loop(void)
 
 const char *generate_fen(const game_st *p_game)
 {
-    char *fen = fen_buf;
+    char *fen = ac_fen_buf;
     uint8_t empty = 0;
     uint8_t pos = a8;
 
-    memset(fen_buf, 0, sizeof(fen_buf));
+    memset(ac_fen_buf, 0, sizeof(ac_fen_buf));
     while(pos <= h1)
     {
         uint8_t piece = p_game->board[pos];
@@ -443,7 +582,12 @@ const char *generate_fen(const game_st *p_game)
     }
 
     *fen++ = ' ';
-    *fen++ = '-'; // todo: ep
+    if (!p_game->stats.ep_square) {
+        *fen++ = '-'; // todo: ep
+    } else {
+        *fen++ = 'a' + FILE(p_game->stats.ep_square);
+        *fen++ = '0' + 8 - RANK(p_game->stats.ep_square);
+    }
 
     *fen++ = ' ';
     fen += snprintf(fen, 4, "%u", p_game->stats.half_moves);
@@ -451,7 +595,7 @@ const char *generate_fen(const game_st *p_game)
     *fen++ = ' ';
     fen += snprintf(fen, 4, "%u", p_game->stats.move_number);
 
-    return fen_buf;
+    return ac_fen_buf;
 }
 
 const char *color_to_string(uint8_t b_color)
--- src/globals.h
+++ src/globals.h
@@ -8,7 +8,7 @@
 
 // app firmware info
 #define K_APP_NAME                  "Electronic Chess Board"
-#define K_APP_VERSION               "01.04.11"
+#define K_APP_VERSION               "01.04.12"
 
 
 // for esp32-s2: RX1=18, TX1=17
